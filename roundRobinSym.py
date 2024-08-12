c1490f765a4925c909078fcefc5e61126dcfa20b
Pedro Greco <rgreco.pedro@gmail.com>
2024-08-12 15:06:17 -0300

    Created a python file, added the basic variables

---
+++ roundRobinSym.py
@@ -0,0 +1,23 @@
+#defines the procecees that will 'happen'
+#processes = [arrival time, execution time]
+processes = [[],[],[],[],[]] 
+
+p1 = [0, 1]
+p2 = [0,2]
+p3 = [1,1]
+p4 = [0,15]
+p5 = [2,10]
+
+#flag variable
+completion = 0
+
+#defines the time in wich the symulkation will occur
+#all the time units are seconds
+time = 0
+overhead = 1
+quantum = 2
+
+while completion !=5:
+
+
+
