5e84eddd0f0530789f06cd5e7f75b8fd1c89d629
Pedro Greco <rgreco.pedro@gmail.com>
2024-08-12 16:21:02 -0300

    Corrected a variable

--- roundRobinSym.py
+++ roundRobinSym.py
@@ -1,12 +1,6 @@
 #defines the procecees that will 'happen'
 #processes = [arrival time, execution time]
-processes = [[],[],[],[],[]] 
-
-p1 = [0, 1]
-p2 = [0,2]
-p3 = [1,1]
-p4 = [0,15]
-p5 = [2,10]
+processes = [[0, 1],[0,2],[1,1],[0,15],[2,10]] 
 
 #flag variable
 completion = 0
@@ -18,6 +12,3 @@ overhead = 1
 quantum = 2
 
 while completion !=5:
-
-
-
