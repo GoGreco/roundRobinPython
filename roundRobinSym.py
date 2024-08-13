f1ce9a4d8f601c1709401b72c0460f203d0b4be3
Pedro Lucas Rodrigues Greco <rgreco.pedro@dinfor.uniceub>
2024-08-13 11:36:47 -0300

    Paused the queue funtion to run a test

--- roundRobinSym.py
+++ roundRobinSym.py
@@ -1,6 +1,32 @@
 #defines the procecees that will 'happen'
 #processes = [arrival time, execution time]
-processes = [[0, 1],[0,2],[1,1],[0,15],[2,10]] 
+class Processes():
+
+    processesList = []
+
+    # creates the object and adds them to a list
+    def __init__(self, arrivalTime, execTime ):
+        self.arrivalTime  = arrivalTime
+        self.execTime = execTime
+        Processes.processesList.append(self)
+
+    #sorts the list by arrival time
+    def queueMaker():
+        list = Processes.processesList
+        queue = sorted_list=sorted(list, key=lambda list : list[0])
+        return queue
+        
+            
+
+#defining processes
+p1m  = Processes(0, 1) 
+p2m  = Processes(0, 2)
+p3m  = Processes(1,1)
+p4ma = Processes(0,15)
+p5ma = Processes(2,10)
+
+
+
 
 #flag variable
 completion = 0
@@ -10,5 +36,9 @@ completion = 0
 time = 0
 overhead = 1
 quantum = 2
-;
+
+
+    
+
 while completion !=5:
+    if processes[*,0]<= quantum:
