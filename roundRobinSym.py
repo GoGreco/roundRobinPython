e16eaa6d31be4bf1a510a03583a8207849151065
Pedro Greco <rgreco.pedro@gmail.com>
2024-08-22 18:32:40 -0300

    made the code more polished(?)

--- roundRobinSym.py
+++ roundRobinSym.py
@@ -1,5 +1,5 @@
-#defining processes (processName, arrivalTime, execTime, first time being executed)
-#need waiting time
+#defining processes (processName, arrivalTime, ramainingExecTime, first time being executed)
+
 p1 = ['p1', 0, 1, 0]
 p2 = ['p2', 0, 2, 0]
 p3 = ['p3', 1, 1, 0]
@@ -13,11 +13,11 @@ returnTime = []
 #queue generation
 queue = [p1, p2, p3, p4, p5]
 
-# Sort the queue based on the first value of each tuple
+#Sort the queue based on the first value of each tuple
 queue = sorted(queue, key=lambda x: x[1])
 
 #flag variable
-completion = 0
+completed = 0
 
 #defines the time in wich the simulation will occur
 #all the time units are seconds
@@ -26,58 +26,62 @@ overhead = 1
 quantum = 2
 n = 0
 
-while completion != 5:
-    
-    #gives the reponse time comparing the current Time - arrivalTime(queue[n][1]). 
-    #queue[n][3]works as a flag to see if the process has entered the simulation before
-    if queue[n][3] == 0: 
+while completed != 5:
+
+    #check the flag value in queue[*][3], p1 to p5, if it's 0 it adds the process to the response time list
+    #response time is then calculated like time subtracted by the arrival time of the proccess in the cpu 
+    if queue[n][3] == 0:
+        responseTime.append([queue[n][0], time - queue[n][1]])
         queue[n][3] = 1
-        responseTime.append([queue[n][0], time-queue[n][1]])
 
-    #checking if the execTime in less than the quantum (but still more than 0),
-    #it than, subtracts the execTime by itself, and adds to completion
-    #also the return time is calculated (Final execTime - arrivalTime)
-    if  0<queue[n][2]<quantum:
-        queue[n][2] -= queue[n][2]
+    #if a process execTime is less than the quantum, than this is added to the chronological time and the execTime becomes 0
+    #also it generates an overhead since the cpu has to switch proccesses
+    if 0 < queue[n][2] < quantum:
         time += queue[n][2]
-        time += overhead
-        completion += 1
-        returnTime.append([queue[n][0], time-queue[n][1]])
+        queue[n][2] = 0
+        completed += 1
 
-    elif queue[n][2]>= quantum:
-        queue[n][2] -=quantum 
+        #as soon as the process finishes,  a return time is generated, by subtracting the time by the proccess arrival time
+        returnTime.append([queue[n][0], time - queue[n][1]])
+
+        time += overhead
+    
+    #if a procces remainingExecTime is more than the quantum, or equal to it, we will subtract the quantum from that remainingExecTime
+    #and add the quantum to the time, since it's equal to the amount of time the proccess expent on the cpu
+    #we will also check if the proccess has concluded
+    elif queue[n][2] >= quantum:
         time += quantum
-        time += overhead
+        queue[n][2] -= quantum
 
-        #when the execTime becomes 0, the return time is calculated (Final execTime - arrivalTime)
-        # it also increasses the flag variable in 1
         if queue[n][2] == 0:
-            completion += 1
-            returnTime.append([queue[n][0], time-queue[n][1]])
+            completed += 1
+
+            #as soon as the process finishes,  a return time is generated, by subtracting the time by the proccess arrival time
+            returnTime.append([queue[n][0], time - queue[n][1]])
+        
+        time += overhead
 
     n+=1
-    if n >=5:
+    if n >= 5:
         n = 0
 
 
-#calculates the waitingTime of each program by compairing the returnTime and the responseTime
-#also calculates the avarage for each return and response times
-sumResponse = 0
-sumReturn = 0
+sumResponse = 0 
+sumReturn = 0 
+for n in range(len(responseTime)):
+    waitingTime.append([queue[n][0], returnTime[n][1] - responseTime[n][1]])
+    
+    sumResponse+=responseTime[n][1]
+    sumReturn+=returnTime[n][1]
+
+responseAverage = sumResponse/len(responseTime)
+returnAverage = sumReturn/len(returnTime)
 
 
-for x in range(len(responseTime)):
-    waitingTime.append([queue[x][0], returnTime[x][1] - responseTime[x][1]])
-    sumResponse += responseTime[x][1]
-    sumReturn += returnTime[x][1]
-
-averageResponse = sumResponse/5
-averageReturn = sumReturn/5
-
-
-print("Resultados para o Round Robin: ")
-print(f"A ordem de atendimento foi: {queue[0][0]}->{queue[1][0]}->{queue[2][0]}->{queue[3][0]}->{queue[4][0]};")
-print(f"Os tempos de resposta foram: {responseTime[0]}, {responseTime[1]}, {responseTime[3]}, {responseTime[2]}, {responseTime[4]}")
-print(f"Os tempos de retorno foram: {returnTime[0]}, {returnTime[1]}, {returnTime[3]}, {returnTime[2]}, {returnTime[4]}")
-print(f"A média dos tempos de resposta foi: {averageResponse}")
-print(f"A media dos tempo de retorno é de: {averageReturn}")
+print("p1->p2->p4->p3->p5")
+print(f'''Os tempos de resposta foram: {responseTime}''')
+print(f'''Os tempos de retorno foram: {returnTime}''')
+print(f'''Os tempos de espera foram: {waitingTime}''')
+print(f'''A média dos tempos de resposta foi: {responseAverage}''')
+print(f'''A média dos tempos de retorno foi: {returnAverage}''' )
+      
